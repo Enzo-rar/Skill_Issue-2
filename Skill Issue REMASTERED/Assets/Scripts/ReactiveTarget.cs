3e9946123a49070a2285d425675b0ec737b125c1
JackElSimple <miguel.angel.puchalt@gmail.com>
2025-11-09 18:28:44 +0100

    cambios ok
    todos ok

---
+++ Skill Issue REMASTERED/Assets/Scripts/ReactiveTarget.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+
+public class ReactiveTarget : MonoBehaviour
+{
+    public void ReactToHit()
+    {
+        Destroy(GetComponent<WanderingAI>());
+        StartCoroutine(Die());
+    }
+
+    private IEnumerator Die()
+    {
+        transform.Rotate(-75, 0, 0);
+
+        yield return new WaitForSeconds(1.5f);
+
+        Destroy(gameObject);
+    }
+
+
+}
