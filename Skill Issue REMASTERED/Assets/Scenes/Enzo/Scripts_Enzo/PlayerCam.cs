b72087811446c91201445e60c4353e6ef7b3d5fc
Enzo-rar <enzosabatin@gmail.com>
2025-11-09 23:07:22 +0100

    Movimiento primera instancia
    Movimiento basado en físicas, con salto funcional sobre superficies con el layer isGround. Prefab de player creado, debería de estar disponible para probar mapas y saltos.
    
    Hay que arreglar un error que hace que el personaje se quede atascado al chocar contra paredes, pero el resto debería de funcionar perfectamente
    
    PD: Para hacer el salto más fluido, he cambiado la gravedad del proyecto de -9.81 a -30

---
+++ Skill Issue REMASTERED/Assets/Scenes/Enzo/Scripts_Enzo/PlayerCam.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+
+    public float sensX, sensY;
+    float xRotation, yRotation;
+
+    public Transform orientation;
+    
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation -= Mathf.Clamp(xRotation, -90, 90);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+}
