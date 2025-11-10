58b6b9ae50babab95d8a63c6ef063930cd41958d
JackElSimple <miguel.angel.puchalt@gmail.com>
2025-11-10 15:36:11 +0100

    salto incorporado a mi rama
    salto aunque demasiado floaty

--- Skill Issue REMASTERED/Assets/Scripts/FPSInput.cs
+++ Skill Issue REMASTERED/Assets/Scripts/FPSInput.cs
@@ -3,24 +3,55 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))] // obliga a que el GameObject tenga cierto componente
 public class FPSInput : MonoBehaviour
 {
-    public float speed = 6.0f;
-    public float gravity = -9.8f;
+	[Header("Movimiento")]
+	public float speed = 6.0f;
+	[Header("Salto y gravedad")]
+	public float jumpHeight = 1.5f;
+	public float gravity = -9.8f;
+
     private CharacterController _charController;
-    void Start()
+	private Vector3 velocity;
+	private bool isGrounded;
+
+	void Start()
     {
         _charController = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        float deltaX = Input.GetAxis("Horizontal"); // Las teclas asociadas están en:
-        float deltaZ = Input.GetAxis("Vertical");   // Edit\Project Settings\Input
-        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-        movement = Vector3.ClampMagnitude(movement, 1.0f) * speed;
-        movement.y = gravity;
+		// --- Movimiento del plano horizontal ---
 
-        movement = transform.TransformDirection(movement); // convierte desde el sistema local al global
-        _charController.Move(movement * Time.deltaTime); // no movemos el transform para que se calculen
+		float deltaX = Input.GetAxis("Horizontal"); // Las teclas asociadas están en:
+        float deltaZ = Input.GetAxis("Vertical");   // Edit\Project Settings\Input
+       // Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+		Vector3 move = transform.right * deltaX + transform.forward * deltaZ;
+		//movement = Vector3.ClampMagnitude(movement, 1.0f) * speed;
+		move = Vector3.ClampMagnitude(move, 1f) * speed;
+		//movement.y = gravity;
+
+		// --- Estado del suelo ---
+		isGrounded = _charController.isGrounded;
+
+		if (isGrounded && velocity.y < 0)
+			velocity.y = -2f; // mantener pegado al suelo
+
+		// --- Salto ---
+		if (Input.GetButtonDown("Jump") && isGrounded)
+		{
+			velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+		}
+
+		// --- Gravedad ---
+		velocity.y += gravity * Time.deltaTime;
+
+		// --- Movimiento total ---
+		Vector3 finalMove = move + Vector3.up * velocity.y; 
+
+		_charController.Move(finalMove * Time.deltaTime);
+
+		//movement = transform.TransformDirection(movement); // convierte desde el sistema local al global
+       // _charController.Move(movement * Time.deltaTime); // no movemos el transform para que se calculen
     }						// las colisiones
 }
 
