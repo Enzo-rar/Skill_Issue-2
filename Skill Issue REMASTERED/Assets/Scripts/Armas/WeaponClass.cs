2c6c18dc53690d0d84c82f2cb8c123dbf25bbda8
Tripa9 <jlloalc@etsinf.upv.es>
2025-12-14 21:05:02 +0100

    Update WeaponClass.cs
    La escopeta ya es más correcta

--- Skill Issue REMASTERED/Assets/Scripts/Armas/WeaponClass.cs
+++ Skill Issue REMASTERED/Assets/Scripts/Armas/WeaponClass.cs
@@ -1,22 +1,28 @@
 
-using System.Linq;
-using UnityEngine;
 using System.Collections;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem.Android;
+using static UnityEngine.GraphicsBuffer;
 
 public class WeaponClass : MonoBehaviour
 {
     private Camera _camera;
+
+    //public Arma arma;
     public int damage;
     public int bulletXShot;
     public float bulletCooldown;
     public int numberOfBullets;
     public bool isAutomatic;
-
+    
     public Animator anim;
 
     void Start()
     {
         anim = GetComponentInChildren<Animator>();
+        //arma = new Arma(damage, bulletXShot, bulletCooldown, numberOfBullets, isAutomatic);
     }
 
     public void setCamera(Camera c)
@@ -26,16 +32,37 @@ public class WeaponClass : MonoBehaviour
 
     public void shootProjectile()
     {
-        
-        Ray ray = _camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit) ) 
+        if (bulletXShot == 1)
         {
-            var hitobject = hit.transform.gameObject;
-            var target = hitobject.GetComponentInParent<PlayerCharacter>();
-            if (target != null)
+            Ray ray = _camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
             {
-                target.Hurt(damage);
+                var hitobject = hit.transform.gameObject;
+                var target = hitobject.GetComponentInParent<PlayerCharacter>();
+                if (target != null)
+                {
+                    target.Hurt(damage);
+                }
+            }
+        }
+        else
+        {
+            for (int i = 1; i <= bulletXShot; i++)
+            {
+                Vector3 angle = new Vector3(Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f), 0.0f);
+                //Ray ray = _camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+                Ray ray = new Ray(_camera.transform.position, Vector3.Normalize(_camera.transform.forward + angle));
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    var hitobject = hit.transform.gameObject;
+                    var target = hitobject.GetComponentInParent<PlayerCharacter>();
+                    if (target != null)
+                    {
+                        target.Hurt(damage);
+                    }
+                }
             }
         }
     }
